472786efe7086a6fc01c566b895bf2b77a6e05cc
Arthur Zhao <adz6@duke.edu>
2024-04-11 16:45:03 -0400

    transition matrix w/o jail row

--- markov_monopoly.py
+++ markov_monopoly.py
@@ -4,7 +4,7 @@ import numpy as np
 def create_board():
     board = np.zeros((40, 40))
 
-    probabilities = [
+    old_probabilities = [
         0.0278,
         0.0556,
         0.0833,
@@ -16,6 +16,22 @@ def create_board():
         0.0833,
         0.0556,
         0.0278,
+    ]
+
+    # (1/6)^3 chance of rolling 3 pairs in a row --> jail. Refactored probabilities on paper
+
+    probabilities = [
+        0.02753703259259259,
+        0.0556,
+        0.0825120437037037,
+        0.1111,
+        0.1375861088888889,
+        0.1667,
+        0.1375861088888889,
+        0.1111,
+        0.0825120437037037,
+        0.0556,
+        0.02753703259259259,
     ]
 
     monopoly_squares = [
@@ -61,16 +77,53 @@ def create_board():
         "Boardwalk",
     ]
 
-    nonstandard_squares = [2, 7, 17, 22, 30, 33, 36]  # 0-indexed
+    # 2 - community chest, 7 - chance, 17 - community chest, 22 - chance, 30 - jail, 33 - community chest, 36 - chance
+
+    # Community Chest: 1/16 go, 1/16 jail. Subtract (2*1/16)/6 from probabilities, add go/jail:
+    chest_probabilities = [p * (14 / 16) for p in probabilities]
+    chest_squares = [2, 17, 33]
+
+    # Chance: 1/16: boardwalk, Go, Illinois, St. Charles, nearest Utility, Go Back 3, Jail, Reading Railroad, 2/16: nearest Railroad
+    # Subtract (10/16)/11 from probabilities
+    chance_probabilities = [p * (6 / 16) for p in probabilities]
+    chance_squares = [7, 22, 36]
 
     for i in range(40):
-        if i in nonstandard_squares:
-            continue
-        start_index = i + 2
-        for j, prob in enumerate(probabilities):
-            board[i, (start_index + j) % 40] = prob
+        if i in chest_squares:
+            for j, prob in enumerate(chest_probabilities):
+                board[i, (i + j + 2) % 40] = prob
+            board[i, 0] += 215 / 3456
+            board[i, 30] += 215 / 3456
 
-    print(board[-1])
+        elif i in chance_squares:
+            for j, prob in enumerate(chance_probabilities):
+                board[i, (i + j + 2) % 40] = prob
+            board[i, 39] += 1075 / 17280
+            board[i, 0] += 1075 / 17280
+            board[i, 24] += 1075 / 17280
+            board[i, 11] += 1075 / 17280
+            board[i, i - 3] += 1075 / 17280
+            board[i, 30] += 1075 / 17280
+            board[i, 5] += 1075 / 17280
+
+            if i == 7:  # Chance near Oriental
+                board[i, 12] += 1075 / 17280
+                board[i, 15] += 1075 / 8640
+            elif i == 36:  # Chance near Park Place
+                board[i, 12] += 1075 / 17280
+                board[i, 5] += 1075 / 8640
+            else:  # Chance near Kentucky
+                board[i, 28] += 1075 / 17280
+                board[i, 25] += 1075 / 8640
+
+        else:  # Normal Squares
+            for j, prob in enumerate(probabilities):
+                board[i, (i + j + 2) % 40] = prob
+
+        # add (1/216) in the Jail cell (column 30)
+        board[i, 30] += 1 / 216
+
+        print(f"Sum of entries in row {i}: {board[i].sum()}")
 
 
 def transition_matrix():
